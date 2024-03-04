e8aa7025e23f710370c89b5d552c91c7e9f3f65f
DimkoO123 <55223651+DimkoO123@users.noreply.github.com>
2024-03-04 12:42:47 +0000

    Header modified

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.testimonials__slider').slick({
+		arrows:false,
+		dots: true,
+		easing: 'ease',
+		autoplay:false,
+		pauseOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotsHover:true,
+		centerMode:true,
+		centerPadding: '0px',
+	});
+})
